ffa209d586454245a73d58e024f9aa370e8d8cba
chrisjordan87 <chrisjordan@Chriss-MacBook-Pro.local>
2015-01-13 17:24:57 +0000

    human player validation

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -5,7 +5,18 @@ module TTT
     end
 
     def next_move
-      @cli_renderer.get_user_input.to_i
+      invalid_move = true
+      while(invalid_move)
+        user_input = @cli_renderer.get_user_input
+        if(is_move_valid(user_input))
+          invalid_move = false
+        end
+      end
+      user_input.to_i
+    end
+
+    def is_move_valid(user_input)
+      user_input.match(/^(\d)+$/) 
     end
   end
 end
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -2,23 +2,28 @@ require '../lib/human_player.rb'
 
 class Fake_CLI_Renderer
 
-  def set_user_input(mocked_user_input)
+  def set_user_input(*mocked_user_input)
     @user_input = mocked_user_input
   end
 
   def get_user_input
-    @user_input
+    @user_input.shift
   end
 end
 
 describe TTT::HumanPlayer do
+  let(:renderer){Fake_CLI_Renderer.new}
+  let(:player){TTT::HumanPlayer.new(renderer)}
   it 'gets user input as next move' do
-    renderer = Fake_CLI_Renderer.new
-    player = TTT::HumanPlayer.new(renderer)
-
     fake_user_move = '5'
     renderer.set_user_input(fake_user_move)
 
+    expect(player.next_move).to eq(fake_user_move.to_i)
+  end
+
+  it 'invalidates user input if non-number provided' do
+    fake_user_move = '5'
+    renderer.set_user_input('A',fake_user_move) 
     expect(player.next_move).to eq(fake_user_move.to_i)
   end
 end
