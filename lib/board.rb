f2737b94b37627affc4e6df0dd32bf104f4b0006
chrisjordan87 <chrisjordan@Chriss-MacBook-Pro.local>
2015-01-14 14:05:32 +0000

    board

--- lib/board.rb
+++ lib/board.rb
@@ -4,11 +4,43 @@ module TTT
       @positions = Array.new(9)
     end
     def add_move(player, position)
-     @positions[position] = player 
+      @positions[position] = player 
     end
 
     def get_player_in_position(position)
       @positions[position]
     end
+
+    def find_winner
+      winner = nil
+      winner = search_for_winner_on_rows || winner
+      winner = search_for_winner_on_columns || winner
+      winner
+    end
+
+    private 
+    def search_for_winner_on_rows
+      search_for_winner_on_lines(get_rows)
+    end
+
+    def search_for_winner_on_columns
+      search_for_winner_on_lines(get_cols)
+    end
+    
+    def search_for_winner_on_lines(lines)
+      winning_line = lines.select do |line|
+        line.all?{|element| element==line.first if line.first}
+      end
+      winning_line[0][0] unless winning_line.empty?
+    end
+
+    def get_cols
+      get_rows.transpose
+    end
+
+    def get_rows
+      @positions.each_slice(3).to_a
+    end
+
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,58 @@
-require '../lib/board.rb'
+require_relative '../lib/board.rb'
 
 describe TTT::Board do
-  it 'marks board with player move' do
-    board = TTT::Board.new
-    player = 'some player'
-    board.add_move(player, 0)
-    expect(board.get_player_in_position(0)).to be(player)
+  let(:board) { TTT::Board.new }
+  PLAYER = 'some player'
+
+  it 'marks board with player move 0' do
+    player_move = 0
+    board.add_move(PLAYER, player_move)
+    expect(board.get_player_in_position(player_move)).to be(PLAYER)
+  end
+  
+  it 'marks board with player move 8' do
+    player_move = 8
+    board.add_move(PLAYER, player_move)
+    expect(board.get_player_in_position(player_move)).to be(PLAYER)
+  end
+
+  it 'board returns nil if no winner on board' do
+    expect(board.find_winner).to be nil
+  end
+
+  it 'board returns winner if top row is occupied by player' do
+    add_moves_to_board(0,1,2)
+    expect(board.find_winner).to be PLAYER
+  end
+
+  it 'board returns winner if middle row is occupied by player' do
+    add_moves_to_board(3,4,5)
+    expect(board.find_winner).to be PLAYER
+  end
+  
+  it 'board returns winner if bottom row is occupied by player' do
+    add_moves_to_board(6,7,8)
+    expect(board.find_winner).to be PLAYER
+  end
+
+  it 'board returns winner if left column is occupied by player' do
+    add_moves_to_board(0,3,6)
+    expect(board.find_winner).to be PLAYER
+  end
+
+  it 'board returns winner if middle column is occupied by player' do
+    add_moves_to_board(1,4,7)
+    expect(board.find_winner).to be PLAYER
+  end
+
+  it 'board returns winner if right column is occupied by player' do
+    add_moves_to_board(2,5,8)
+    expect(board.find_winner).to be PLAYER
+  end
+
+  def add_moves_to_board(i,j,k)
+    board.add_move(PLAYER,i)
+    board.add_move(PLAYER,j)
+    board.add_move(PLAYER,k)
   end
 end
