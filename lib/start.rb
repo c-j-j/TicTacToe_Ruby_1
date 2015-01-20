493a2d2902a5c98177d81c02d1a60ce6d972c0e0
c-j-j <chrisjordan1987@gmail.com>
2015-01-20 14:46:37 +0000

    Put start script in place

--- lib/start.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'cli_renderer.rb'
-require_relative 'main.rb'
-
-TTT::Main.new(TTT::Display.new).run
-
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -1,27 +1,7 @@
 require_relative '../lib/minimax.rb'
 
-module TTT
-  class TestBoard
-    attr_accessor :game_over_count
-    def initialize
-      @game_over_count = 0
-      @game_over_sequence = []
-    end
-
-    def set_game_over_sequence(*sequence)
-      @game_over_sequence = sequence
-    end
-
-    def game_over?
-      @game_over_count += 1
-      return @game_over_sequence.shift
-    end
-  end
-end
-
 describe TTT::Minimax do
 
-  let(:board) {TTT::TestBoard.new}
   let(:minimax) { TTT::Minimax.new(board) }
 
   #it 'calls terminal state checker' do
